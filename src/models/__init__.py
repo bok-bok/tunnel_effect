e0e0bc94cfb45e7f7e659ab5206155dfa2e6bdd9
Kyungbok Lee <dlrudqhr7180@gmail.com>
2024-01-20 14:11:00 -0500

    add resnet18

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
 from models.models import MLP, ResNet34
 from models.resnet import resnet34
 from models.resnet18 import get_resnet18
+from models.resnet18_imagenet100 import get_resnet18_imagenet100
 from models.vgg import VGG
